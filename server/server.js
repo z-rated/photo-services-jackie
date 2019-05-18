6a4eadba21e01cc5af460c0ff612c61ca53bbacb
eugenia ong <eugenia.osy@gmail.com>
2019-05-17 18:57:17 -0700

    separate app.js and server.js for easier testing

--- server/server.js
+++ server/server.js
@@ -1,25 +1,4 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('../db/schema.js');
+const app = require('./app.js');
 
 const port = 2001;
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/api/restaurants/:id/photos', (req, res) => {
-  db.getPhotos(req.params.id, (err, result) => {
-    if (err) {
-      res.status(404).send();
-    } else {
-      const urls = result.photos;
-      res.status(200).send(urls);
-    }
-  });
-});
-
 app.listen(port, console.log('listening on port', port));
