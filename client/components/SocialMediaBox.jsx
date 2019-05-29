108b3bd9caf2d3b501c3065aab7f801e1c1a75c6
eugenia ong <eugenia.osy@gmail.com>
2019-05-29 10:54:22 -0700

    Start on possible social media box

---
+++ client/components/SocialMediaBox.jsx
@@ -0,0 +1,21 @@
+import react from 'react';
+import SocialMediaBox from '../styled/SocialMediaBox';
+
+export default function SocialMediaBox() {
+  return (
+    <svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink">
+      <title>E-Mail</title>
+      <defs></defs>
+      <g id="Functional" stroke="none" stroke-width="1" fill-rule="evenodd">
+        <g id="social_email" stroke-width="1.2" stroke="#FFFFFF">
+          <g id="Page-1-Copy-3" transform="translate(6.000000, 11.000000)">
+            <polygon id="Stroke-1" points="0 17.6710391 27.0233057 17.6710391 27.0233057 0 0 0"></polygon>
+            <polyline id="Stroke-3" points="27.0233057 1.51868508e-05 13.5706128 10.6064354 0 1.51868508e-05"></polyline>
+            <path d="M0,17.6710391 L7.32323908,10.2980512" id="Stroke-4"></path>
+            <path d="M20.110153,10.6064506 L27.0232827,17.5761517" id="Stroke-5"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
